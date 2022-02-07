8b2324a4cb0fad27c4b4a5a63bc98b1b47279422
Szabó Bence <xszbencex@gmail.com>
2022-02-07 20:34:39 +0100

    Project init, db init

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # calorie-counter-backend
 Backend for my calorie counter app for my thesis.
+
+Adatbázis: PostgreSQL
+Szerver: calorie-counter-db
+Felhasználó: postgres
+Jelszó: admin
+Adatbázis: calorie-counter-db
---
+++ src/main/java/inf/unideb/caloriecounterbackend/CalorieCounterBackendApplication.java
@@ -0,0 +1,13 @@
+package inf.unideb.caloriecounterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieCounterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalorieCounterBackendApplication.class, args);
+	}
+
+}
